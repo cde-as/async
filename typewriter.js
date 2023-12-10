5efed8b1dd0b90e1c2ffd9e894a2ed39c057a34b
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-10 08:48:20 -0800

    practiced writing for of loop instead of the traditional for loop

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,29 @@
 const sentence = "hello there from lighthouse labs";
 
+// USING TRADITIONAL FOR LOOP
 
-for (let i = 0; i < sentence.length; i++) {
+/* for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
     process.stdout.write(sentence[i]);
 
     if (i === sentence.length - 1) {
       console.log("\n");
     }
-    
-  }, i * 50);
-}
 
+  }, i * 50);
+} */
+
+// USING FOR OF LOOP
+
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+
+    if (sentence.indexOf(char) === sentence.length - 1) {
+      process.stdout.write('\n');
+    }
+
+  }, delay += 50);
+}
